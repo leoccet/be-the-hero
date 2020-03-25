f10e8fe06e92a4b78e5d06c88c901b4edb385462
leo_cesar <leoccet@gmail.com>
2020-03-24 22:27:06 -0300

    Commit inicial

---
+++ backend/src/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const routes = require('./routes')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utiliado para criar ou alterar recursos
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
+
+
+
+
+
+
